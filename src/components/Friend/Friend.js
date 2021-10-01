dce14f8a9a42ead746c1328061655f137ba6bfa9
eemon47 <emon.cse36@gmail.com>
2021-10-02 00:13:36 +0600

    first commit

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Friend = (props) => {
+
+    const { id, name, phone, website, address } = props.friend;
+
+    const friendStyle = {
+        border: '3px solid goldenrod',
+        padding: '10px',
+        borderRadius: '10px'
+    };
+    return (
+        <div style = {friendStyle}>
+            <h1>I am {name}</h1>
+            <h4>Connect me {phone}</h4>
+            <h5>Visit me {website}</h5>
+            <p><small>I live in {address.city}</small></p>
+        </div>
+    );
+};
+
+export default Friend;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>page not found</h2>
+            <p>Try again kiddos...!</p>
+        </div>
+    );
+};
+
+export default NotFound;
